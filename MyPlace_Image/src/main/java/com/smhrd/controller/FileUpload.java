04e334daa8daf486e6bb6a03a100149c0a9908c4
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-10 16:30:23 +0900

    deploy

---
+++ MyPlace_Image/src/main/java/com/smhrd/controller/FileUpload.java
@@ -0,0 +1,28 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/FileUpload")
+public class FileUpload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// file 받아오기
+		
+		
+		
+		// 파일 경로( url )
+		// MyPlace_Image/img/_126241775_getty_cats.jpg
+		// 만 돌려주기
+	
+		response.sendRedirect("http://www.naver.com/");
+	
+	}
+
+}
