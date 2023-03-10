9a68baa8a71559a3e47cd380f34b6a0310b62934
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-10 16:44:20 +0900

    deploy

--- MyPlace_Image/src/main/java/com/smhrd/controller/FileUpload.java
+++ MyPlace_Image/src/main/java/com/smhrd/controller/FileUpload.java
@@ -1,6 +1,8 @@
 package com.smhrd.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class FileUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		// file 받아오기
 		
 		
@@ -21,8 +23,11 @@ public class FileUpload extends HttpServlet {
 		// MyPlace_Image/img/_126241775_getty_cats.jpg
 		// 만 돌려주기
 	
-		response.sendRedirect("http://www.naver.com/");
-	
+		// 데이터 응답
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.print("파일url");
 	}
 
 }
