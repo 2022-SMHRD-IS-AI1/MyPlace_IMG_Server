265a0f6a7977cf328b8da03ca83fff066758231c
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-10 16:01:28 +0900

    deploy
    라는 단어가 들어가있으면 서버 시작

---
+++ MyPlace_Image/src/main/java/com/smhrd/controller/TestServlet.java
@@ -0,0 +1,37 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		final String IMAGE = "img/_126241775_getty_cats.jpg";
+	
+		PrintWriter out = response.getWriter();
+		
+		
+		out.print("<h1>배포 테스트하기</h1>");
+	
+		out.print("<a");
+		out.print("href='" + IMAGE +"'");
+		out.print(">");
+		
+		
+		out.print("고양이");
+		out.print("</a>");
+	
+	
+	
+	
+	}
+
+}
