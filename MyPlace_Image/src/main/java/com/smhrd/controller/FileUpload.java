f47dc6724f5659ffe94636c7e93028e349d986d3
이민채 <118702388+lmc1996@users.noreply.github.com>
2023-03-11 16:14:14 +0900

    deploy

--- MyPlace_Image/src/main/java/com/smhrd/controller/FileUpload.java
+++ MyPlace_Image/src/main/java/com/smhrd/controller/FileUpload.java
@@ -1,78 +1,57 @@
 package com.smhrd.controller;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
-//@MultipartConfig(location = "/home/ubuntu/web/apache-tomcat-9.0.73/work/Catalina/localhost/MyPlace_Image/Img")
-@MultipartConfig(
-        location = "/home/ubuntu/web/apache-tomcat-9.0.73/work/Catalina/localhost/MyPlace_Image/Img",
-        maxFileSize = -1,
-        maxRequestSize = -1,
-        fileSizeThreshold = 1024)
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 @WebServlet("/FileUpload")
 public class FileUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String ATTACHES_DIR = "/home/ubuntu/web/apache-tomcat-9.0.73/work/Catalina/localhost/MyPlace_Image/Img";
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		// file 받아오기
+	
+		// 1. 어디에?
+		String savePath = request.getServletContext().getRealPath("img");
 		
-		Part file = request.getPart("myFileInput");
-		String contentType = request.getContentType();
-		InputStream fileContent = file.getInputStream();
+		// 2. file 최대 용량
+		int max = 1024 * 1024 * 5; // byte
+		
+		// 3. 어떤 인코딩?
+		String encoding = "UTF-8";
+	
+		// MultipartRequest객체
+		MultipartRequest multi = new MultipartRequest(request,
+														savePath,
+														max,
+														encoding,
+														new DefaultFileRenamePolicy());
+		
+		// new DefaultFileRenamePolicy() : 중복 이름 처리
+		// cat.jpg // cat2.jpg
+		
+		// file 저장하기
+		// file의 이름을 꺼냈을 때 저장
+		String fileName = multi.getFilesystemName("myFileInput");
+	
+	
+	
+		// 비동기 통신 응답
+		response.setCharacterEncoding("UTF-8");
+		
 		PrintWriter out = response.getWriter();
 		
-        if (contentType != null &&  contentType.toLowerCase().startsWith("multipart/")) {
-        	System.out.printf("파라미터 명 : %s, contentType :  %s,  size : %d bytes \n", file.getName(),
-        			file.getContentType(), file.getSize());
- 
-            if  (file.getHeader("Content-Disposition").contains("filename=")) {
-                String fileName =  extractFileName(file.getHeader("Content-Disposition"));
-                
-                if (file.getSize() > 0) {
-                    System.out.printf("업로드 파일 명 : %s  \n", fileName);
-                    file.write(ATTACHES_DIR + File.separator  + fileName);
-                    file.delete();
-                }
-            } else {
-                String formValue =  request.getParameter(file.getName());
-                System.out.printf("name : %s, value : %s  \n", file.getName(), formValue);
-            }
-        System.out.println("업로드 완료");
-        }
 		
-		String url = "MyPlace_Image/img/"+file.getName();
-		// 파일 경로( url )
-		// MyPlace_Image/img/_126241775_getty_cats.jpg
-		// 만 돌려주기
+		out.print(fileName);
+		
 	
-		// 데이터 응답
-		response.setContentType("text/html; charset=utf-8");
-		
-		
-		out.print(url);
-		
-	}
-		
-	String extractFileName(String partHeader) {
-		for (String cd : partHeader.split(";")) {
-			if (cd.trim().startsWith("filename")) {
-				String fileName = cd.substring(cd.indexOf("=") +  1).trim().replace("\"", "");;
-				int index = fileName.lastIndexOf(File.separator);
-				return fileName.substring(index + 1);
-			}
-		}
-		return null;
+	
 	}
 
-	}
+}
---
+++ MyPlace_Image/src/main/java/com/smhrd/controller/FileUpload3.java
@@ -0,0 +1,78 @@
+package com.smhrd.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+//@MultipartConfig(location = "/home/ubuntu/web/apache-tomcat-9.0.73/work/Catalina/localhost/MyPlace_Image/Img")
+@MultipartConfig(
+        location = "/home/ubuntu/web/apache-tomcat-9.0.73/work/Catalina/localhost/MyPlace_Image/Img",
+        maxFileSize = -1,
+        maxRequestSize = -1,
+        fileSizeThreshold = 1024)
+@WebServlet("/FileUpload2")
+public class FileUpload3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String ATTACHES_DIR = "/home/ubuntu/web/apache-tomcat-9.0.73/work/Catalina/localhost/MyPlace_Image/Img";
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		// file 받아오기
+		
+		Part file = request.getPart("myFileInput");
+		String contentType = request.getContentType();
+		InputStream fileContent = file.getInputStream();
+		PrintWriter out = response.getWriter();
+		
+        if (contentType != null &&  contentType.toLowerCase().startsWith("multipart/")) {
+        	System.out.printf("파라미터 명 : %s, contentType :  %s,  size : %d bytes \n", file.getName(),
+        			file.getContentType(), file.getSize());
+ 
+            if  (file.getHeader("Content-Disposition").contains("filename=")) {
+                String fileName =  extractFileName(file.getHeader("Content-Disposition"));
+                
+                if (file.getSize() > 0) {
+                    System.out.printf("업로드 파일 명 : %s  \n", fileName);
+                    file.write(ATTACHES_DIR + File.separator  + fileName);
+                    file.delete();
+                }
+            } else {
+                String formValue =  request.getParameter(file.getName());
+                System.out.printf("name : %s, value : %s  \n", file.getName(), formValue);
+            }
+        System.out.println("업로드 완료");
+        }
+		
+		String url = "MyPlace_Image/img/"+file.getName();
+		// 파일 경로( url )
+		// MyPlace_Image/img/_126241775_getty_cats.jpg
+		// 만 돌려주기
+	
+		// 데이터 응답
+		response.setContentType("text/html; charset=utf-8");
+		
+		
+		out.print(url);
+		
+	}
+		
+	String extractFileName(String partHeader) {
+		for (String cd : partHeader.split(";")) {
+			if (cd.trim().startsWith("filename")) {
+				String fileName = cd.substring(cd.indexOf("=") +  1).trim().replace("\"", "");;
+				int index = fileName.lastIndexOf(File.separator);
+				return fileName.substring(index + 1);
+			}
+		}
+		return null;
+	}
+
+	}
